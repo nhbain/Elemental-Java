d998c84bfcfc3fbffa657e5b96644e322471c0fe
NHB <bain607@gmail.com>
2017-07-18 08:40:30 -0500

    Initial Commit.

---
+++ Chapter 2/C2_E1.java
@@ -0,0 +1,17 @@
+public class C2_E1{
+
+	public static void print_name(){
+		System.out.println("My name is Austin Akerley");
+	}
+
+	public static void print_learn(){
+		System.out.println("I am going to learn Java Code and build something CRAZYYY.");
+	}
+
+	public static void main( String[] args){
+		print_name();
+		print_learn();
+		System.out.println("Today is the 9th of September, 2015");
+	}
+
+}
