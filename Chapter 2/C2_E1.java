4ee69715fae1173202fa8afed05daf90da3d5916
nhbain <bain607@gmail.com>
2017-07-18 11:37:21 -0500

    Pushing example 2.

--- Chapter 2/C2_E1.java
+++ Chapter 2/C2_E1.java
@@ -2,7 +2,7 @@ public class C2_E1{
 
 	public static void print_name(){
 		System.out.println("My name is Austin Akerley");
-	}
+	
 
 	public static void print_learn(){
 		System.out.println("I am going to learn Java Code and build something CRAZYYY.");
---
+++ Chapter 2/C2_E2.java
@@ -0,0 +1,10 @@
+public class C2_E2
+{
+  public static void main(String[] args)
+  {
+    String statement = "This is a super long string that is going to" +
+    "take up multiple lines. Concatination makes my existence possible!";
+
+    System.out.println(statement);
+  }
+}
